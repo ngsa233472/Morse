c3035390251f34bf2c7c3b72ef76a39e26c68225
Nick Gray <ngsa233472@gmail.com>
2025-08-24 06:17:06 -0500

    Create morse.py

---
+++ morse.py
@@ -0,0 +1,25 @@
+# Morse code map
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    ' ': '/'
+}
+
+def text_to_morse(text):
+    morse_code = []
+    for char in text.upper():
+        if char in MORSE_CODE_DICT:
+            morse_code.append(MORSE_CODE_DICT[char])
+        else:
+            # Unknown character
+            morse_code.append('?')
+    return ' '.join(morse_code)
+
+if __name__ == "__main__":
+    input_text = input("Enter text to convert to Morse code: ")
+    print("Morse Code:", text_to_morse(input_text))
